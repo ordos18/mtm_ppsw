62f7657e12afb2593c7e744e1b4b3c8f36c54850
Maciek <ordos18@gmail.com>
2019-11-29 12:26:46 +0100

    I2C advanced v0.2 lab

--- Current/i2c.c
+++ Current/i2c.c
@@ -7,10 +7,15 @@
 
 #define mI2C_INTERRUPT 0x08
 
-#define mI2C_INTERRUPT_START 			0x08
-#define mI2C_INTERRUPT_SLA_ACK		0x18
-#define mI2C_INTERRUPT_SLA_NACK		0x20
-#define mI2C_INTERRUPT_DATA_ACK		0x28
+#define mI2C_INTERRUPT_START 					0x08
+#define mI2C_INTERRUPT_SLA_W_ACK			0x18
+#define mI2C_INTERRUPT_SLA_W_NACK			0x20
+#define mI2C_INTERRUPT_DATA_TX_ACK		0x28
+
+#define mI2C_INTERRUPT_SLA_R_ACK			0x40
+#define mI2C_INTERRUPT_SLA_R_NACK			0x48
+#define mI2C_INTERRUPT_DATA_RX_ACK		0x50
+#define mI2C_INTERRUPT_DATA_RX_NACK		0x58
 
 // VIC (Vector Interrupt Controller) VICIntEnable
 #define VIC_I2C_CHANNEL_NR 9
@@ -18,26 +23,43 @@
 // VICVectCntlx Vector Control Registers
 #define mIRQ_SLOT_ENABLE 0x00000020
 
-unsigned char I2CAddress, I2CData;
+unsigned char I2CAddress, I2CDataTx, I2CDataRx, I2CDone;
+struct I2C_Params sI2C_Params;
 
 void (*ptrI2CInterruptFunction)(void);
 
 //(Interrupt Service Routine) of I2C interrupt
-__irq void I2CIRQHandler(void){
+__irq void I2CIRQHandler (void){
 	
 	switch (I2STAT) {
 		case (mI2C_INTERRUPT_START):
 			I2CONCLR = 0x20;							// Clear start bit
 			I2DAT = I2CAddress;						// Send address and write bit
 			break;
-		case (mI2C_INTERRUPT_SLA_ACK):
-			I2DAT = I2CData;							// Write data to TX register
+		case (mI2C_INTERRUPT_SLA_W_ACK):
+			I2DAT = I2CDataTx;							// Write data to TX register
 			break;
-		case (mI2C_INTERRUPT_SLA_NACK):
+		case (mI2C_INTERRUPT_SLA_W_NACK):
 			I2DAT = I2CAddress;						// Resend address and write bit
 			break;
-		case (mI2C_INTERRUPT_DATA_ACK):
+		case (mI2C_INTERRUPT_DATA_TX_ACK):
 			I2CONSET = 0x10;							// Stop condition
+			I2CDone = 1;
+			break;
+		
+		case (mI2C_INTERRUPT_SLA_R_ACK):
+			I2CONSET = 0x04;							// Enable ACK for data byte
+			break;
+		case (mI2C_INTERRUPT_SLA_R_NACK):
+			I2CONSET = 0x20;							// Resend Start condition
+			break;
+		case (mI2C_INTERRUPT_DATA_RX_ACK):
+			I2CDataRx = I2DAT;
+			I2CONSET = 0x10;							// Stop condition
+			I2CDone = 1;
+			break;
+		case (mI2C_INTERRUPT_DATA_RX_NACK):
+			I2CONSET = 0x20;							// Resend Start condition
 			break;
 		default:
 			break;
@@ -47,7 +69,7 @@ __irq void I2CIRQHandler(void){
 	VICVectAddr = 0x00; 				// potwierdzenie wykonania procedury obslugi przerwania
 }
 
-void I2CInterrupts_Init(void (*ptrInterruptFunction)(void)) {
+void I2CInterrupts_Init (void (*ptrInterruptFunction)(void)) {
 	
 	ptrI2CInterruptFunction = ptrInterruptFunction;
 	
@@ -55,7 +77,7 @@ void I2CInterrupts_Init(void (*ptrInterruptFunction)(void)) {
 
 	VICIntEnable |= (0x1 << VIC_I2C_CHANNEL_NR);            // Enable I2C interrupt channel 
 	VICVectCntl4  = mIRQ_SLOT_ENABLE | VIC_I2C_CHANNEL_NR;  // Enable Slot 4 and assign it to I2C interrupt channel
-	VICVectAddr4  =(unsigned long)I2CIRQHandler; 	   				// Set to Slot 4 Address of Interrupt Service Routine 
+	VICVectAddr4  = (unsigned long)I2CIRQHandler; 	   				// Set to Slot 4 Address of Interrupt Service Routine 
 }
 
 void I2C_Init (void) {
@@ -70,33 +92,88 @@ void I2C_Init (void) {
 	VICIntEnable |= (0x1 << VIC_I2C_CHANNEL_NR);            // Enable I2C interrupt channel 
 	VICVectCntl4  = mIRQ_SLOT_ENABLE | VIC_I2C_CHANNEL_NR;  // Enable Slot 4 and assign it to I2C interrupt channel
 	VICVectAddr4  =(unsigned long)I2CIRQHandler; 	   				// Set to Slot 4 Address of Interrupt Service Routine 
+	
+	sI2C_Params.ucDone = 1;
 }
 
-void PCF8574_Write(unsigned char ucData) {
+void I2C_Start (void) {
 	
-	I2CAddress = 0x40;
-	I2CData = ucData;
 	I2CONCLR = 0x6C;
 	I2CONSET = 0x40;
 	I2CONSET = 0x20;
-	
 }
 
-void I2C_ExecuteTransaction(struct I2C_Params sTParams) {
+void I2C_ExecuteTransaction (void) {
 	
-	switch(sTParams.eI2CTransmissionMode) {
+	unsigned char ucIterator;
+	
+	sI2C_Params.ucDone = 0;
+	I2CAddress = sI2C_Params.ucSlaveAddress;
+	
+	switch(sI2C_Params.eI2CTransmissionMode) {
 		case TX:
-			// TODO
+			for(ucIterator = 0; ucIterator < sI2C_Params.ucNrOfBytesForTx; ucIterator++) {
+				I2C_Start();
+				I2CDataTx = *(sI2C_Params.pucBytesForTx);
+				sI2C_Params.pucBytesForTx++;
+				while(I2CDone != 1) {}
+				I2CDone = 0;
+			}
 			break;
 		case RX:
-			
+			for(ucIterator = 0; ucIterator < sI2C_Params.ucNrOfBytesForRx; ucIterator++) {
+				I2C_Start();
+				*(sI2C_Params.pucBytesForRx) = I2CDataRx;
+				sI2C_Params.pucBytesForRx++;
+				while(I2CDone != 1) {}
+				I2CDone = 0;
+			}
 			break;
 		case RX_AFTER_TX:
-			
+			for(ucIterator = 0; ucIterator < sI2C_Params.ucNrOfBytesForTx; ucIterator++) {
+				I2C_Start();
+				I2CDataTx = *(sI2C_Params.pucBytesForTx);
+				sI2C_Params.pucBytesForTx++;
+				while(I2CDone != 1) {}
+				I2CDone = 0;
+			}
+			for(ucIterator = 0; ucIterator < sI2C_Params.ucNrOfBytesForRx; ucIterator++) {
+				I2C_Start();
+				*(sI2C_Params.pucBytesForRx) = I2CDataRx;
+				sI2C_Params.pucBytesForRx++;
+				while(I2CDone != 1) {}
+				I2CDone = 0;
+			}
 			break;
 		case TX_AFTER_RX:
 			break;
 		default:
 			break;
 	}
+	
+	
+}
+
+void PCF8574_Write (unsigned char ucData) {
+	
+	sI2C_Params.eI2CTransmissionMode = TX;
+	sI2C_Params.ucSlaveAddress = 0x40;
+	sI2C_Params.pucBytesForTx = &ucData;
+	sI2C_Params.ucNrOfBytesForTx = 1;
+	sI2C_Params.pucBytesForRx = 0;
+	sI2C_Params.ucNrOfBytesForRx = 0;
+	
+	I2C_ExecuteTransaction();
+}
+
+void PCF8574_Read (void) {
+	
+	sI2C_Params.eI2CTransmissionMode = RX;
+	sI2C_Params.ucSlaveAddress = 0x41;
+	sI2C_Params.pucBytesForTx = 0;
+	sI2C_Params.ucNrOfBytesForTx = 0;
+	sI2C_Params.pucBytesForRx = 0;
+	sI2C_Params.ucNrOfBytesForRx = 1;
+	
+	I2C_ExecuteTransaction();
 }
